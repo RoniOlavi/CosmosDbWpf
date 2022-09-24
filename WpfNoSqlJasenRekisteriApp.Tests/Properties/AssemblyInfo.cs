753ada46974c6a923d813c048f33d336237439af
Roni Rissanen <roni.rissanen@student.careeria.fi>
2022-09-24 21:24:28 +0300

    lisätty testit

---
+++ WpfNoSqlJasenRekisteriApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WpfNoSqlJasenRekisteriApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WpfNoSqlJasenRekisteriApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0b99ab3-1e3c-4b3a-84d0-354865a70084")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WpfNoSqlJasenRekisteriApp.Tests/UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WpfNoSqlJasenRekisteriApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public string EndpointUri { get; set; } = "<URI>";
+        private string PrimaryKey { get; set; } = "<PRIMARYKEY>";
+
+        [TestMethod]
+        public void TestGetDocuments()
+        {
+            Uri uri = new Uri(EndpointUri);
+            DocumentClient client = new DocumentClient(uri, PrimaryKey);
+            //Arrange
+            Uri kokoelmaUrl = UriFactory.CreateDocumentCollectionUri("ToDoList", "Items");
+            var queryable = client.CreateDocumentQuery<Jasen>(kokoelmaUrl, new FeedOptions { MaxItemCount = 100 }).Where(b => b.PartitionKey > 0).AsDocumentQuery();
+            var results = queryable.HasMoreResults;
+            var total = client.CreateDocumentQuery<Jasen>(kokoelmaUrl, "SELECT * FROM C").ToList();
+            //Act
+            if (results == false)
+            {
+                throw new Exception("Ei hakenut dokumenttia kannasta");
+            }
+            else
+            {
+                Assert.IsNotNull(results);
+                Assert.AreEqual(5, total.Count);
+            } 
+        }
+
+
+        [TestMethod]
+        public void TestAddDocument()
+        {
+            //Arrange
+            Uri uri = new Uri(EndpointUri);
+            DocumentClient client = new DocumentClient(uri, PrimaryKey);
+            Uri kokoelmaUrl = UriFactory.CreateDocumentCollectionUri("ToDoList", "Items");
+            var results = client.CreateDocumentQuery<Jasen>(kokoelmaUrl, "SELECT * FROM C").ToList();
+            Jasen jasen = new Jasen()
+            {
+                Id = "123456",
+                PartitionKey = 123,
+                Etunimi = "Teppo",
+                Sukunimi = "Testaaja",
+                Osoite = "Oikopolku 10",
+                Puhelin = "1234567",
+                Sahkoposti = "testi.testi@suomi.fi",
+                Postinumero = 48100,
+                Alkupvm = DateTime.Now
+            };
+            //Act
+            var dokumentti = client.UpsertDocumentAsync(kokoelmaUrl, jasen);
+            var results2 = client.CreateDocumentQuery<Jasen>(kokoelmaUrl, "SELECT * FROM C").ToList();
+            //Assert
+            if (dokumentti == null)
+            {
+                Assert.Fail("Käyttääjää ei luotu");
+            }
+            else
+            {
+                Assert.AreNotEqual(results.Count, results2.Count);
+            }
+        }
+
+        [TestMethod]
+        public async Task TestDeleteDocument()
+        {
+            //Arrange
+            Uri uri = new Uri(EndpointUri);
+            DocumentClient client = new DocumentClient(uri, PrimaryKey);
+            //Act
+            string id = "123456";
+            int avain = 123;
+            Uri dokumenttiUrl = UriFactory.CreateDocumentUri("ToDoList", "Items", id);
+            _ = await client.DeleteDocumentAsync(dokumenttiUrl,
+                new RequestOptions { PartitionKey = new PartitionKey(avain) });
+
+            Uri kokoelmaUrl = UriFactory.CreateDocumentCollectionUri("ToDoList", "Items");
+            var results = client.CreateDocumentQuery<Jasen>(kokoelmaUrl, "SELECT * FROM C").ToList();
+            
+            //Assert
+            Assert.AreEqual(5, results.Count);
+        }
+    }
+}
